4daca1e4a4a641175078306a0819201251ee6b86
summer_64 <xiashuocheng64@gmail.com>
2022-09-09 18:13:30 +0800

    feat: 完成数据面板界面部分内容

--- src/pages/dataPanel/DataPanelComponent.tsx
+++ src/pages/dataPanel/DataPanelComponent.tsx
@@ -5,20 +5,68 @@ import {Row, Col} from 'antd'
 // type
 import type {FunctionComponent} from 'react'
 
-import {createGaugeChartData} from './utils'
+import {createGaugeChartData, createBarChartData, createLineChartData} from './utils'
+import classNames from 'classnames'
 
-const DataPanelComponent: FunctionComponent<any> = props => (
-  <section className={styles['container']}>
-    <Row gutter={12} className={styles['row-1']}>
-      {[1, 2, 4].map(el => (
-        <Col span={'6'} className={styles['chart-container']} key={el}>
-          <ReactEcharts style={{width: '100%', height: '100%'}} option={createGaugeChartData()} />
+const DataPanelComponent: FunctionComponent<any> = props => {
+  const renderPhaseLighningStroke = (data: {name: string; value: string}) => {
+    const {name = '-', value = '-'} = data
+    return (
+      <section className={styles['phase-item']} key={name}>
+        <p className={classNames(styles['phase-title'], styles[`color-${name}`])}>{`${name}相`}</p>
+        <div className={styles['phase-value-container']}>
+          <span className={styles['phase-value']}>{`${value}次`}</span>
+        </div>
+      </section>
+    )
+  }
+
+  return (
+    <section className={styles['container']}>
+      <Row gutter={12} className={styles['row-1']}>
+        {[1, 2, 4].map(el => (
+          <Col span={'6'} className={styles['chart-container']} key={el}>
+            <ReactEcharts style={{width: '100%', height: '100%'}} option={createGaugeChartData()} />
+          </Col>
+        ))}
+        <Col span={'6'} className={styles['lighning-stroke-container']}>
+          <div className={styles['lighning-stroke-item']} />
+          <span className={styles['title']}>雷击次数</span>
+          <span className={styles['count']}>4次</span>
         </Col>
-      ))}
-
-      <Col span={'6'}></Col>
-    </Row>
-  </section>
-)
+      </Row>
+      <Row gutter={12} className={styles['row-2']}>
+        <Col span={'6'} className={styles['col-1']}>
+          <span className={styles['title']}>泄露电量最新值</span>
+          <ReactEcharts style={{width: '100%', height: '95%'}} option={createBarChartData()} />
+        </Col>
+        <Col span={'6'} className={styles['col-2']}>
+          <span className={styles['title']}>雷击累计次数</span>
+          <section className={styles['lighning-stroke-count-container']}>
+            {[
+              {name: 'A', value: '6'},
+              {name: 'B', value: '3'},
+              {name: 'C', value: '1'},
+            ].map(el => renderPhaseLighningStroke(el))}
+          </section>
+        </Col>
+        <Col span={'12'} className={styles['col-3']}>
+          <p className={classNames(styles['left-title'], styles['base-title'])}>泄露电流历史状态(mA)</p>
+          <ReactEcharts style={{width: '100%', height: '100%'}} option={createLineChartData()} />
+        </Col>
+      </Row>
+      {/* <Row gutter={12} className={styles['row-3']}>
+      <Col span={'6'} className={styles['chart-container']}>
+        <ReactEcharts style={{width: '100%', height: '100%'}} option={createBarChartData()} />
+      </Col>
+      <Col span={'6'} className={styles['lighning-stroke-container']}>
+        <div className={styles['lighning-stroke-item']} />
+        <span className={styles['title']}>雷击次数</span>
+        <span className={styles['count']}>4次</span>
+      </Col>
+    </Row> */}
+    </section>
+  )
+}
 
 export default DataPanelComponent
--- src/pages/dataPanel/utils.ts
+++ src/pages/dataPanel/utils.ts
@@ -2,17 +2,55 @@ import {EChartsOption} from 'echarts-for-react'
 import * as echarts from 'echarts'
 
 const createGaugeChartData = (): EChartsOption => {
+  const axisLineColor = new echarts.graphic.LinearGradient(0, 1, 0, 0, [
+    {offset: 1, color: '#18C8FF'},
+    {offset: 0, color: '#18C8FF00'},
+  ])
+  const ProgressColor = new echarts.graphic.LinearGradient(0, 1, 0, 0, [
+    {offset: 0, color: '#5C55E9'},
+    {offset: 1, color: '#19C5FE'},
+  ])
+  const gradientRampWarm = new echarts.graphic.LinearGradient(1, 0, 0, 0, [
+    {offset: 0, color: '#033D71'},
+    {offset: 1, color: '#592F35'},
+  ])
+  const unit = '%'
+  const generalOptions = {
+    name: '',
+    type: 'gauge',
+    startAngle: 180,
+    center: ['50%', '90%'],
+    endAngle: 0,
+    axisLine: {
+      show: false,
+    },
+    splitLine: {
+      show: false,
+    },
+    axisTick: {
+      show: false,
+    },
+    axisLabel: {
+      show: false,
+    },
+    detail: {
+      show: false,
+    },
+  }
   const option = {
     tooltip: {
       formatter: '{a} <br/>{b} : {c}%',
     },
     series: [
+      // 外层刻度 图标名称 指针
       {
+        z: 10,
         name: 'Pressure',
         type: 'gauge',
         startAngle: 180,
         endAngle: 0,
-        radius: '120%',
+        // 仪表盘半径，可以是相对于容器高宽中较小的一项的一半的百分比，也可以是绝对的数值。
+        radius: '140%',
 
         center: ['50%', '90%'],
 
@@ -21,92 +59,231 @@ const createGaugeChartData = (): EChartsOption => {
           textBorderColor: 'FFB24D',
           textShadowColor: '#FFB24D',
           textShadowBlur: 10,
-          offsetCenter: ['-100%', '-100%'],
+          offsetCenter: ['-90%', '-100%'],
         },
-        detail: {
-          show: false,
-        },
+
         axisLine: {
           show: true,
-          lineStyle: {width: 1},
-        },
-        progress: {
-          show: true,
-          width: 10,
-          itemStyle: {
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              {offset: 0, color: '#18C8FF'},
-              {offset: 1, color: '#5C53DE'},
-            ]),
+          lineStyle: {
+            color: [[1, axisLineColor]],
+            width: 1,
           },
         },
         splitLine: {
           show: false,
         },
         axisTick: {
-          distance: -30,
+          distance: 0,
         },
+        axisLabel: {
+          distance: -10,
+        },
+        detail: {
+          show: false,
+        },
+
         data: [
           {
-            value: 50,
+            value: '73',
             name: 'SCORE',
             itemStyle: {
-              color: 'red',
+              color: 'white',
             },
           },
         ],
       },
-      // {
-      //   type: 'gauge',
-      //   startAngle: 180,
-      //   endAngle: 0,
+      // 内层真实数据进度 单位
+      {
+        ...generalOptions,
+        z: 100,
+        // 仪表盘半径，可以是相对于容器高宽中较小的一项的一半的百分比，也可以是绝对的数值。
+        radius: '110%',
+        title: {
+          color: '#fff',
+          offsetCenter: ['0%', '-75%'],
+        },
+        progress: {
+          show: true,
+          width: 8,
+          itemStyle: {
+            color: ProgressColor,
+          },
+        },
+        data: [
+          {
+            value: '73',
+            name: unit,
+            itemStyle: {
+              color: 'transparent',
+            },
+          },
+        ],
+      },
+      // 较宽的装饰环
+      {
+        ...generalOptions,
 
-      //   center: ['50%', '90%'],
-      //   radius: '75%',
-      //   title: {
-      //     color: '#FFB24D ',
-      //     textBorderColor: 'FFB24D',
-      //     textShadowColor: '#FFB24D',
-      //     textShadowBlur: 10,
-      //     offsetCenter: ['-100%', '-100%'],
-      //   },
-      //   detail: {
-      //     show: false,
-      //   },
-      //   axisLine: {
-      //     show: true,
-      //     lineStyle: {width: 1},
-      //   },
-      //   progress: {
-      //     show: true,
-      //     width: 10,
-      //     itemStyle: {
-      //       color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-      //         {offset: 0, color: '#18C8FF'},
-      //         {offset: 1, color: '#5C53DE'},
-      //       ]),
-      //     },
-      //   },
-      //   splitLine: {
-      //     show: false,
-      //   },
-      //   axisTick: {
-      //     distance: 20,
-      //     splitNumber: 10,
-      //     color: 'red',
-      //   },
-      //   data: [
-      //     {
-      //       value: 50,
-      //       itemStyle: {
-      //         color: 'transparent',
-      //       },
-      //     },
-      //   ],
-      // },
+        // 仪表盘半径，可以是相对于容器高宽中较小的一项的一半的百分比，也可以是绝对的数值。
+        radius: '95%',
+
+        progress: {
+          show: true,
+          width: 30,
+          itemStyle: {
+            color: gradientRampWarm,
+          },
+        },
+
+        data: [
+          {
+            value: '100',
+            itemStyle: {
+              color: 'transparent',
+            },
+          },
+        ],
+      },
+      // 最内部的装饰线
+      {
+        ...generalOptions,
+        // 仪表盘半径，可以是相对于容器高宽中较小的一项的一半的百分比，也可以是绝对的数值。
+        radius: '55%',
+
+        progress: {
+          show: true,
+          width: 1,
+          itemStyle: {
+            color: gradientRampWarm,
+          },
+        },
+
+        data: [
+          {
+            value: '100',
+            itemStyle: {
+              color: 'transparent',
+            },
+          },
+        ],
+      },
+    ],
+  }
+  return option
+}
+const createBarChartData = (): EChartsOption => {
+  const unit = '%'
+
+  const option = {
+    xAxis: {
+      type: 'category',
+      position: 'top',
+
+      data: ['A相', 'B相', 'C相'],
+      axisLine: {show: false},
+      axisLabel: {show: true},
+      axisTick: {show: false},
+    },
+    yAxis: {
+      type: 'value',
+      axisLine: {show: false},
+      axisLabel: {show: false},
+      splitLine: {show: false},
+    },
+
+    series: [
+      {
+        data: ['0.08', '0.1', '0.25'],
+        type: 'bar',
+        barWidth: '45',
+        showBackground: true,
+        label: {
+          show: true,
+          color: '#fff',
+          fontWeight: 'bold',
+          formatter: (val: any) => val.value + 'mA',
+
+          position: 'bottom',
+        },
+        backgroundStyle: {
+          color: 'rgba(180, 180, 180, 0.2)',
+          borderColor: '#A3DBEA',
+          borderWidth: 1,
+        },
+        itemStyle: {
+          color: '#A3DBEA',
+          borderWidth: 1,
+        },
+      },
+    ],
+  }
+  return option
+}
+const createLineChartData = (): EChartsOption => {
+  const unit = '%'
+
+  const option = {
+    title: {
+      text: 'Stacked Line',
+    },
+    tooltip: {
+      trigger: 'axis',
+    },
+    legend: {
+      data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine'],
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true,
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {},
+      },
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        name: 'Email',
+        type: 'line',
+        stack: 'Total',
+        data: [120, 132, 101, 134, 90, 230, 210],
+      },
+      {
+        name: 'Union Ads',
+        type: 'line',
+        stack: 'Total',
+        data: [220, 182, 191, 234, 290, 330, 310],
+      },
+      {
+        name: 'Video Ads',
+        type: 'line',
+        stack: 'Total',
+        data: [150, 232, 201, 154, 190, 330, 410],
+      },
+      {
+        name: 'Direct',
+        type: 'line',
+        stack: 'Total',
+        data: [320, 332, 301, 334, 390, 330, 320],
+      },
+      {
+        name: 'Search Engine',
+        type: 'line',
+        stack: 'Total',
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+      },
     ],
   }
   return option
 }
 
-export {createGaugeChartData}
+export {createGaugeChartData, createBarChartData, createLineChartData}
